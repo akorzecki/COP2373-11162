75f0c0b15bd4ed598550fda7c50b97671acb8af8
ChefAnthonyy <130565793+fodtony1@users.noreply.github.com>
2026-08-17 14:41:07 -0400

    Edited Docstring to Tech Design Doc Generator

--- Directions and Examples/docstring to tech design doc txt generator.py
+++ Directions and Examples/docstring to tech design doc txt generator.py
@@ -4,7 +4,7 @@ import docstring_example # replace with your assignment name (without .py)
 with open("docstring_example_design_doc.txt", "w") as doc:
     doc.write(f"# Technical Design Document: {docstring_example.__name__}\n\n")
     #replace with your name, the date, and the description of the program
-    doc.write(f"# Name: Susan Melichar\n")
+    doc.write(f"# Name: Anthony Korzecki\n")
     doc.write(f"# Date: November 12, 2025\n")
     doc.write(f"# Program Description: Give brief description of your program\n\n")
     #replace docstring_example with your assignment name
@@ -12,5 +12,5 @@ with open("docstring_example_design_doc.txt", "w") as doc:
         doc.write(f"## Function: {name}\n")
         doc.write(f"{inspect.getdoc(func)}\n\n")
     #replace with link to your repository
-    doc.write(f"#Link to your repository: https://github.com/melichs?tab=repositories")
+    doc.write(f"#Link to your repository: https://github.com/akorzecki/COP2373-11162")
 print('Complete')
